e83e9ed31fd4d1ab1aa791e6ccaa92fe9b350d14
Harry van Engelen <harry.v.engelen@gmail.com>
2024-01-06 10:04:43 +0100

    Restructure

--- main.py
+++ main.py
@@ -1,67 +1,71 @@
-#from machine import Pin, I2C, ADC
 import machine
 import time
 import network
 import ahtx0
-#import ubinascii
 import json
 
 from mqttsimple import MQTTClient
 
+def read_sensor():
+    i2c = machine.I2C(0, scl=machine.Pin(22), sda=machine.Pin(21))
+    sensor = ahtx0.AHT10(i2c)
+    return sensor.temperature, sensor.relative_humidity
+
+def read_vbat():
+    vbat_adc = machine.ADC(machine.Pin(35))
+    vbat_adc.atten(machine.ADC.ATTN_11DB)       #Full range: 3.3v
+    return (vbat_adc.read_uv() * 2) / 1000000.0
+
 print("\n\n\n")
 print("start\n")
 
-#led = machine.Pin(5, machine.Pin.OUT)
-i2c = machine.I2C(0, scl=machine.Pin(22), sda=machine.Pin(21))
-sensor = ahtx0.AHT10(i2c)
+try:
+    temp, hum = read_sensor()
+    print("Temperature: %0.2f C" % temp)
+    print("Humidity: %0.2f %%" % hum)
 
-vbat = machine.ADC(machine.Pin(35))
-vbat.atten(machine.ADC.ATTN_11DB)       #Full range: 3.3v
+    vbat = read_vbat()
+    print("Vbat: %0.2f %%" % vbat)
 
-print("Init done, start network")
-t_start = time.ticks_ms()
+    print("Init done, start network")
+    t_start = time.ticks_ms()
 
-wlan = network.WLAN(network.STA_IF) # create station interface
-wlan.active(True)       # activate the interface
-#print(wlan.scan())             # scan for access points
-wlan.connect('Harrys Wlan', 'Hallo, weer Harry1 dus') # connect to an AP
+    wlan = network.WLAN(network.STA_IF) 
+    wlan.active(True)
+    wlan.config(dhcp_hostname = "mobsens")
+    wlan.connect('Harrys Wlan', 'Hallo, weer Harry1 dus')
 
-print("Wait while connected")
-timo = 50
-while not wlan.isconnected() and timo > 0:
-    time.sleep(.1)
-    timo -= 1
-if timo <= 0:
-    print("Cannot connect to network")
-    machine.deepsleep(60000)
+    print("Wait while connected")
+    timo = 50
+    while not wlan.isconnected() and timo > 0:
+        time.sleep(.1)
+        timo -= 1
+    if timo <= 0:
+        raise Exception("Cannot connect to network")
 
-t_nwk_connected = time.ticks_ms()
+    t_nwk_connected = time.ticks_ms()
 
-print("Connection successful")
-#print(wlan.ifconfig())
+    print("Connection successful")
 
-#mqtt_server = 'mqtt.harry.thuis'
-mqtt_server = '192.168.62.99'
-print("Connect to mqtt broker")
-client = MQTTClient("umqtt_client", mqtt_server)
+    mqtt_server = 'mqtt.harry.thuis'
+    #mqtt_server = '192.168.62.99'
+    print("Connect to mqtt broker")
+    client = MQTTClient("umqtt_client", mqtt_server)
 
-#led.value(not led.value())
-print("\n")
-print("Temperature: %0.2f C" % sensor.temperature)
-print("Humidity: %0.2f %%" % sensor.relative_humidity)
-vbat_value = (vbat.read_uv() * 2) / 1000000.0
-print("Vbatt: %0.2f %%" % vbat_value)
+    client.connect()
+    t_mqtt_connected = time.ticks_ms()
+    msg = "T=%0.2f C, H=%0.2f%%, V=%0.2f, %d %d %d" % (temp, hum, vbat, t_start, t_nwk_connected, t_mqtt_connected)
+    print("Connected, send message; " + msg)
+    print("%d %d %d" % (t_start, t_nwk_connected, t_mqtt_connected))
+    client.publish(b"mobsens", msg, qos=1)
+    t_end = time.ticks_ms()
+    msg = "End=%d" % t_end
+    client.publish(b"mobsens", msg, qos=1)
+    print("%d" % (t_end))
+    client.disconnect()
+    #time.sleep(1)
+except Exception as e:
+    print(e)
 
-client.connect()
-t_mqtt_connected = time.ticks_ms()
-msg = "T=%0.2f C, H=%0.2f%%, V=%0.2f, %d %d %d" % (sensor.temperature, sensor.relative_humidity, vbat_value, t_start, t_nwk_connected, t_mqtt_connected)
-print("Connected, send message; " + msg)
-print("%d %d %d" % (t_start, t_nwk_connected, t_mqtt_connected))
-client.publish(b"mobsens", msg, qos=1)
-t_end = time.ticks_ms()
-msg = "End=%d" % t_end
-client.publish(b"mobsens", msg, qos=1)
-print("%d" % (t_end))
-client.disconnect()
-#time.sleep(1)
+print("Welterusten")        
 machine.deepsleep(60000)
